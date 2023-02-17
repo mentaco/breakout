531876584516a4ed864a7992ad3ca56514fd389d
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2023-02-17 12:02:06 +0900

    Controllerのインスタンス生成，余計なコードを削除

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 #include "conroller.h"
 
 int main(void){
-    int result;
     initscr();
     noecho();
     curs_set(0);
-    Paddle paddle;
+    Controller controller;
+
     while (1){
         int ch = getch();
         if (ch == 'q') break;
-        result = paddle.sum_x(100);
+
+        controller.update();
+        controller.draw();
     }
     endwin();
 }
