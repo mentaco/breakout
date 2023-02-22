a2f532e9eac8dd160730bd4965f26ecaea7f1de1
Takuro Inada <107355417+mentaco@users.noreply.github.com>
2023-02-22 21:44:43 +0900

    パドルの動きの修正

--- paddle.cpp
+++ paddle.cpp
@@ -3,7 +3,7 @@
 #include "constants.h"
 
 Paddle::Paddle()
-    : row(ROW_SIZE), column(COLUMN_SIZE), v(3)
+    : row(ROW_SIZE), column(COLUMN_SIZE / 2 - 3), v(3)
 {
     
 }
@@ -13,7 +13,7 @@ void Paddle::movement(int ch){
     else if (ch == 'd') column += v;
 
     if (column < 0) column = 0;
-    else if (ROW_SIZE < column + 5) column = ROW_SIZE - 5;
+    else if (COLUMN_SIZE < column + 5) column = COLUMN_SIZE - 5;
 }
 
 void Paddle::draw(){
